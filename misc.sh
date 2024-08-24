f0655cbab0b5fe97623d2bbd9ac84f9400cbbc17
Binh Phan <ptbinh2003@gmail.com>
2024-08-25 05:21:39 +0700

    add install for systemctl, IBUS input method

--- misc.sh
+++ misc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install -y file man htop powertop acpi bash-completion
+sudo apt install -y file man htop powertop acpi bash-completion systemctl
 
 grep -q "powertop --auto-tune" /etc/rc.local ||
 sudo sh -c "echo powertop --auto-tune >> /etc/rc.local"
--- xfce.sh
+++ xfce.sh
@@ -4,12 +4,14 @@ sudo apt install -y --no-install-recommends \
   xinit xserver-xorg elogind policykit-1 \
   xfce4-session xfwm4 xfce4-panel xfce4-terminal \
   xfce4-power-manager xfce4-notifyd gnome-icon-theme \
-  pulseaudio xfce4-pulseaudio-plugin
+  pulseaudio xfce4-pulseaudio-plugin \
+  ibus-unikey im-config ibus-gtk3
 
 echo "#!/bin/bash
 rm -rf ~/.dbus/
 rm -rf ~/.cache/sessions/
 rm -rf ~/.config/pulse/
+rm -rf ~/.config/ibus/
 " > clean
 
 echo "#!/bin/bash
